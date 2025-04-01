348aaa70c5a2df5ee0120cb46a7a734a9008be15
ablant17 <ablant17@charlotte.edu>
2025-04-01 13:44:29 -0400

    Update recommendation_system.ads

--- movie system/recommendation_system.ads
+++ movie system/recommendation_system.ads
@@ -1,9 +1,19 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package Recommendation_System is
    procedure Initialize;
-   procedure Get_Recommendation (Genre : in String; Duration : in String; Popularity : in String; Service : in String);
-   procedure Add_To_Watchlist (Movie : in String);
+
+   procedure Get_Recommendation
+     (Genre    : in String;
+      Duration : in String;
+      Popularity : in String;
+      Service  : in String);
+
+   procedure Add_To_Watchlist (Movie_Title : in String);
+   procedure Remove_From_Watchlist (Movie_Title : in String);
    procedure Display_Watchlist;
-   procedure Remove_From_Watchlist (Movie : in String);
-   procedure Search_Movie (Title : in String);
+   procedure Search_Movie (Movie_Title : in String);
    procedure Random_Movie;
+   function Fetch_From_API (URL : in String) return Unbounded_String;
+
 end Recommendation_System;
