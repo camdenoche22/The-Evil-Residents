1e2656b452cd3eb87e07da19bb158f63cbe28dc1
ablant17 <ablant17@charlotte.edu>
2025-04-01 19:58:48 -0400

    Create movie_db.ads

---
+++ src/movie_db.ads
@@ -0,0 +1,10 @@
+package Movie_DB is
+
+   procedure Load_Database;
+   procedure Recommend_By_Genre;
+   procedure Recommend_By_Duration;
+   procedure Recommend_By_Platform;
+   procedure Recommend_By_Popularity;
+   procedure Random_Movie;
+
+end Movie_DB;
